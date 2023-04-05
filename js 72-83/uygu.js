2d277a9de1fb35d27cfcae71ae6565ccc6e05313
liodark <122795254+liodark@users.noreply.github.com>
2023-04-05 17:27:10 +0500

    Add files via upload

---
+++ js 72-83/uygu.js
@@ -0,0 +1,92 @@
+// 72
+// 73
+
+// let test = 32342;
+// if (test > 10) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// let test = 3;
+// if (test < 10) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// let test = 10;
+// if (test >= 10) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// let test = 3;
+// if (test <= 10) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 76
+
+// let test1 = 1;
+// let test2 = 2;
+// if (test2 > test1) {
+//     console.log('+++'); 
+// } else {
+//     console.log('---');
+// }
+
+// let test1 = 2;
+// let test2 = 2;
+// if (test2 == test1) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 77
+
+// let test1 = 'abc';
+// if (test1 == 'abc') {
+// 	console.log('+++'); 
+// } else {
+// 	console.log('---');
+// }
+
+// 78
+
+// let test1 = '123';
+// let test2 = 123;
+// if (test1 == test2){
+//     console.log('+++')
+// } else{
+//     console.log('---')
+// }
+
+// 79
+
+// let num = 3;
+// if (num > 0 && num < 5) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// let num = 15
+// if(num >= 10 && num <= 20){
+//     console.log('+++')
+// }else{
+//     console.log('---')
+// }
+
+// let num1 = 1;
+// let num2 = 3;
+
+// if (num1 <= 2 && num2 >= 3) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
